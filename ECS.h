24c8c8ad5ec426c071ebe6fbe04b2d07277f7931
Aaron-Kavanaugh <aaron.kavanaugh@outlook.com>
2025-06-04 18:07:52 -0500

    Updated Pool class

--- ECS.h
+++ ECS.h
@@ -30,6 +30,8 @@ public:
 
 	int GetId() const;
 
+	bool operator ==(const Entity& other) const { return id == other.id; };
+
 private:
 	int id;
 };
@@ -53,13 +55,56 @@ private:
 	std::vector<Entity> entities;
 };
 
+template <typename T>
+class Pool
+{
+public:
+	
+	Pool(int size = 100)
+	{
+		data.reserve(size);
+	}
+
+	~pool() = default;
+
+	bool isEmpty() const
+	{
+		return data.empty();
+	}
+
+	int GetSize() const
+	{
+		return data.size();
+	}
+
+	void Resize(int n)
+	{
+		data.resize(n);
+	}
+
+	void Clear()
+	{
+		data.clear();
+	}
+
+	void Add(T object)
+	{
+		data.push_back(object);
+	}
+
+private:
+	std::vector<T> data;
+};
+
 class Registry
 {
 public:
-
+	
+	
 
 private:
-
+	int numEntites = 0;
+	std::vector<Pool*> componentPools;
 };
 
 template <typename TComponent>
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Pikuma - Mystic Woods
+
+Follow along project with Pikuma's 2D Game Engine with C++ and Lua. This project serves as an introduction to Data-Oriented Design (DOD) principles, well teaching me
+basic game engine design as well.
+
+# A few differences
+- I am using SDL3 instead of SDL2 as used by the original course
+- Replacing using provided game assets with my own, sourced from itch.io
+
+https://pikuma.com/courses/cpp-2d-game-engine-development link to Pikuma's website
+
+# A few things covered by the project
+- basic 2D rendering and input handling
+- Entity-component-system (ECS)
+- Lau scripting language integration
+- Game loops and system management
+- Asset loading 
+
+# links
+- https://pikuma.com/
+- https://game-endeavor.itch.io/mystic-woods
+ 
